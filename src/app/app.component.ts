becdc4a9801f83eff409303963f95bb72ff13cd4
oliverpk2000 <oliver.pecek@gmail.com>
2022-11-09 19:51:45 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todolist';
+  inputBox: string = 'inputBox';
+  entryName: string = "";
+  entryFinishBy: Date = new Date();
+  toDoList: Entry[] = []
+  todoStyle: string = "todo";
+  entryNotes: string = "";
+  showNotes: boolean = false;
+
+  createEntry() {
+    this.toDoList.push({name: this.entryName, finishBy:this.entryFinishBy, notes: this.entryNotes});
+    this.entryName = "";
+    this.entryFinishBy = new Date();
+    this.entryNotes = "";
+  }
+
+  delete(index:number) {
+    this.toDoList.splice(index, 1)
+  }
+
+  changeNotesState() {
+    this.showNotes = !this.showNotes;
+  }
+}
+
+class Entry{
+  name: string = "";
+  finishBy: Date = new Date();
+  notes: string = "";
+}
